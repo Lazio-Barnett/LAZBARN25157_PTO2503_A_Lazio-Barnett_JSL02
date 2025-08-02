e5471bc68474aa564f47bf46610cd1a3887d7d62
Lazio Barnett <101427039+Lazio-Barnett@users.noreply.github.com>
2025-08-02 21:28:45 +0200

    Fixed the the input on scripts.js

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+// user input
+
+let userTask_1 = prompt("");
+let userTaskDescription = prompt("");
+let taskStatus = prompt(
+  "Please confirm the task status: 'todo', 'doing', 'done'"
+);
+
+function taskChecker() {
+  if (taskStatus === done) console.log("You got it to work");
+}
